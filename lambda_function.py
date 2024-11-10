e890c034f44229587b89826f0863b824ae003551
Kevin Wang <k499wang@uwaterloo.ca>
2024-11-10 00:05:36 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,17 +6,17 @@ table = dynamodb.Table('Cloud-Resume-Database')
 
 def lambda_handler(event, context):
     # Retrieve user IP address from CloudFront headers
-    user_ip = event['headers'].get('x-forwarded-for')
-    print(user_ip)
+    user_ip = event['headers'].get('x-forwarded-for', "127.0.0.1")
+    print(user_ip) # To see the full event, return event.json.dumps()
 
-    # Check if the IP already exists in the database with a different ID (e.g., '2')
+    # Check if the IP already exists in the database with a different ID
     ip_response = table.get_item(Key={'id': '2'})
     ip_list = ip_response.get('Item', {}).get('ips', [])
 
     if user_ip not in ip_list:
         # If IP is unique, increment views and add IP to the list
         views_response = table.get_item(Key={'id': '1'})
-        views = views_response['Item']['views'] + 1
+        views = views_response['Item']['views'] + 1 # Should probably use get instead ... 
 
         # Update views count in the database
         table.put_item(Item={'id': '1', 'views': views})
